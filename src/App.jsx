8e2fd94a4107987954c5875c66a2d6dbcb6a92a0
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-10-05 13:48:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div  className='text-center '>
+        <h2 className='text-5xl mt-96 text-green-500 font-semibold'>Tech Titan</h2>
+        <p className='text-slate-500 mt-3 '>Your trusted Gadget Shop with every electronics you need</p>
+      </div>
+    </>
+  )
+}
+
+export default App
