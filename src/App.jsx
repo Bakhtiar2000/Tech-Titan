378d4345405765588c7695689421c201cb93b05f
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-10-05 13:56:57 +0600

    new branch created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
 
   return (
     <>
+      {/* Initializing Github */}
       <div  className='text-center '>
         <h2 className='text-5xl mt-96 text-green-500 font-semibold'>Tech Titan</h2>
         <p className='text-slate-500 mt-3 '>Your trusted Gadget Shop with every electronics you need</p>
