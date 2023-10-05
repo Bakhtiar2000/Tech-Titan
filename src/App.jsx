d9bed21eaae2ec3090e517019802ac5477fa1d0f
omarfarukee <umarfaruque1920@gmail.com>
2023-10-05 21:00:04 +0600

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ function App() {
   return (
     <>
       <div  className='text-center '>
-        <h2 className='text-5xl mt-96 text-green-500 font-semibold'>Tech Titan</h2>
-        <p className='text-slate-500 mt-3 '>Your trusted Gadget Shop with every electronics you need</p>
+        <h2 className='text-5xl font-semibold text-green-500 mt-96'>Tech Titan</h2>
+        <h1>hiiii</h1>
+        <p className='mt-3 text-slate-500 '>Your trusted Gadget Shop with every electronics you need</p>
       </div>
     </>
   )
