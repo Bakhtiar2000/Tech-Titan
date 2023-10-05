49d797cc161baa49168d62c137f266e8f61363ab
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-10-05 17:36:49 +0600

    created branch fahim

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,17 @@ function App() {
       <div  className='text-center '>
         <h2 className='text-5xl mt-96 text-green-500 font-semibold'>Tech Titan</h2>
         <p className='text-slate-500 mt-3 '>Your trusted Gadget Shop with every electronics you need</p>
+
+        
+        <h2 className='text-xl font-bold mt-5'>Avengers, Assemble:</h2>
+        <ul>
+          <li>Captain Fahim on your service sir.</li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
       </div>
+
     </>
   )
 }
